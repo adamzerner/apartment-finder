339691659f444b6dc3af10c8df1bf2e303d6caef
Adam Zerner <azerner3@gmail.com>
2017-04-29 16:18:33 -0700

    Clean up price code using promises.

--- utilities.js
+++ utilities.js
@@ -1,69 +1,65 @@
 var cheerio = require('cheerio');
-var rp = require('request-promise');
-var async = require('async');
+var request = require('request-promise');
+var asyncP = require('async-promises');
 
 module.exports = {
-  getApartmentPrices: getApartmentPrices,
+  getApartmentRentPrices: getApartmentRentPrices,
 };
 
-function getApartmentPrices(apartments, res) {
-  async.each(apartments, function (apartment, callback) {
-    getApartmentPrice(apartment)
-      .then(function () {
-        console.log(apartment.name + ' price: ', apartment.price);
-        callback(null, apartment);
-      })
-      .catch(function (err) {
-        console.error('error with ' + apartment.name);
-        callback('error');
-      })
-    ;
-  }, function (error, apartments) {
-    console.log('apartments: ', apartments);
-    res.json(apartments);
+function getApartmentRentPrices(apartments) {
+  return asyncP.each(apartments, function (apartment) {
+    return getApartmentRentPrice(apartment);
   });
 }
 
-function getApartmentPrice(apartment) {
+function getApartmentRentPrice(apartment) {
   return getApartmentFinderDotComUrl(apartment.name)
     .then(function (apartmentFinderDotComUrl) {
-      return rp(apartmentFinderDotComUrl);
+      return request(apartmentFinderDotComUrl);
     })
-    .then(function (body) {
-      var $ = cheerio.load(body);
-      var price = $('.rent').first().text().trim();
-      price = price.split(' - ')[0]; // if it's a range, take lower end of range
-      price = price.slice(1); // remove $ from eg. '$800'
-      price = price.replace(',', ''); // remove ','
-      price = Number(price);
-      apartment.price = price;
+    .catch(function () {
+      console.error('Wrong apartmentfinder.com url.');
+      return null;
+    })
+    .then(function (apartmentFinderDotComHTML) {
+      if (!apartmentFinderDotComHTML) {
+        return apartment;
+      }
+
+      var $ = cheerio.load(apartmentFinderDotComHTML);
+      var rent = $('.rent').first().text().trim();
+      rent = rent.split(' - ')[0]; // if it's a range, take lower end of range
+      rent = rent.slice(1); // remove $ from eg. '$800'
+      rent = rent.replace(',', ''); // remove ','
+      rent = Number(rent);
+      apartment.rent = rent;
+      return apartment;
     })
   ;
 }
 
 function getApartmentFinderDotComUrl(apartmentName) {
-  return rp
+  return request
     .post('http://www.apartmentfinder.com/api/geography/search/', {
       json: {
-        t: apartmentName + ' las vegas',
+        t: apartmentName,
         l: [-115.23835, 36.25502],
       }
     })
-    .then(function (err, response, body) {
-      var name;
-
-      if (body && body[0]) {
-        name = body[0].Address.BuildingName;
-      } else {
-        name = apartmentName
-                .split(' ')
-                .map(function (str) {
-                  return str.charAt(0).toUpperCase() + str.slice(1);
-                })
-                .join('-')
-              ;
-      }
-
+    .then(function (body) {
+      return body[0].Address.BuildingName;
+    })
+    .catch(function () {
+      console.error('Error with apartmentfinder.com autocomplete.');
+      return apartmentName
+               .split(' ')
+               .map(function (str) {
+                 return str.charAt(0).toUpperCase() + str.slice(1);
+               })
+               .join('-')
+             ;
+    })
+    .then(function (name) {
       return 'http://www.apartmentfinder.com/Nevada/Las-Vegas-Apartments/' + name + '-Apartments';
     })
   ;
